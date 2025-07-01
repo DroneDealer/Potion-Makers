ac3ea4839d55c37bfcf5728dfd3875d7c3a0dbff
DroneDealer <1rastogiani@gmail.com>
2025-07-01 10:28:17 -0400

    in the process of creating and coding ingredient info panels

---
+++ Assets/IngredientInfo.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class IngredientInfo
+{
+    public string IngredientName;
+    public string Ingredientdescription;
+    public Sprite IngredientIcon;
+}
