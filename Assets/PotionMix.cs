9ed452189e6c65ff4f5fc57466b6bd58726b0319
DroneDealer <1rastogiani@gmail.com>
2025-06-30 22:35:29 -0400

    committing another hour of  Unity because I can't track unity hours

--- Assets/PotionMix.cs
+++ Assets/PotionMix.cs
@@ -5,7 +5,11 @@ public class PotionMix : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Committing 2 horus of Unity because the extensions are not working
+        //Committing another hour of Unity because the extensions are not working. 
+        // just to clarify: in the last 2 hour update, i was making a bakground, adding sprites, and making animations.
+        // In this 1 hour update, I was animating more sprites, adding more assets, and finishing thier hitboxes
+
+
     }
 
     // Update is called once per frame
